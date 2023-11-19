d499bc55812633ecf6b1a0607c768070f90c02ec
SushankSinha <SushankSinha>
2023-11-19 14:34:30 +0530

    auth

--- router/authentication.js
+++ router/authentication.js
@@ -65,10 +65,6 @@ router.post("/login", async (req, res) => {
 export const authenticate = (req, res, next) => {
   const token = req.cookies.token;
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
   jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Forbidden" });
