a2a39f38badb0659f9acc358a243dab6a034b6f3
SushankSinha <SushankSinha>
2023-11-19 11:08:28 +0530

    api

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect(dataBase, {useUnifiedTopology : true,
 
 app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: ["https://notebook-client.netlify.app"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
